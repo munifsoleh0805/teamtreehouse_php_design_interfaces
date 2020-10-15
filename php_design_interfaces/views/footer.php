fdd97ef4a12781a436fc0d7acaa15e9602eeb756
munifsoleh0805 <70565611+munifsoleh0805@users.noreply.github.com>
2020-10-15 13:44:15 +0700

    project finish

---
+++ php_design_interfaces/views/footer.php
@@ -0,0 +1,9 @@
+</div>
+
+<footer class="footer">
+    <div class="container">
+        <p class="text-center text-muted">by Munif Soleh at Treehouse &copy;2020</p>
+    </div>
+</footer>
+</body>
+</html>
